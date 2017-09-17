69c3cceafb0c9ee1d4292e9fdcf0341500c81e68
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:34:16 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,17 +2,17 @@ from conans import ConanFile, tools, os
 
 class BoostQvmConan(ConanFile):
     name = "Boost.Qvm"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-qvm"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["qvm"]
-    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Exception/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.64.0@bincrafters/testing"
+    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Exception/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.65.1@bincrafters/testing"
 
                       #assert1 core2 exception5 static_assert1 throw_exception2
 
