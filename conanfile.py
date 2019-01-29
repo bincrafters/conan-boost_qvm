0ea6c489ac010c38a3bc1434873cbef02d30542b
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:07 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostQvmConan(base.BoostBaseConan):
     name = "boost_qvm"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_qvm"
     lib_short_names = ["qvm"]
     header_only_libs = ["qvm"]
