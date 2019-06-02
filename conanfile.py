920a3834a2547127d76717d64891d1f241454d69
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:07 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,18 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostQvmConan(base.BoostBaseConan):
     name = "boost_qvm"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_qvm"
-    lib_short_names = ["qvm"]
-    header_only_libs = ["qvm"]
-    b2_requires = [
-        "boost_assert",
-        "boost_core",
-        "boost_exception",
-        "boost_static_assert",
-        "boost_throw_exception"
-    ]
+    version = "1.70.0"
